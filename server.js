bb4f845abd4d33da532f6a70a1c89da98a0240ab
LabaDev <labalimbu360@gmail.com>
2023-01-10 15:57:07 +0000

    added authors section

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+const dotenv = require('dotenv')
 if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+    dotenv.config()
 }
-
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
+mongoose.set('strictQuery', true)
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -14,14 +18,13 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
 
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 
 app.listen(process.env.PORT || 3001)
