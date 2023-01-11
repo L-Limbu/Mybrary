d1c1e5e70db5862836ea43f83d47d34f617c3365
LabaDev <labalimbu360@gmail.com>
2023-01-11 20:15:01 +0000

    File Upload Database

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,11 @@
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+)
+FilePond.setOptions({
+    stylePanelAspectRatio: 1.5,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+})
+FilePond.parse(document.body)
